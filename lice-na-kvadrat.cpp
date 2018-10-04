79012817a733d409404468b72f8e77103e61fb80
tuni97 <43601294+tuni97@users.noreply.github.com>
2018-10-04 09:08:22 +0300

    Add files via upload

---
+++ lice-na-kvadrat.cpp
@@ -0,0 +1,11 @@
+ //lice na kvadrat
+ #include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin>>a;
+    int lice=a*a;
+    cout<<lice;
+    return 0;
+}
---
+++ lice-na-trapec.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+      double b1,b2,h;
+      cin>>b1>>b2>>h;
+      double Lice;
+      cin>>Lice;
+      Lice=((b1+b2)*h)/2
+      cout<<"Lice="<<Lice;
+      return 0;
+}
