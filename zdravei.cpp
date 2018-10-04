eed44157d203d56b80d79534a2ee9e8bf8e2f52a
tuni97 <43601294+tuni97@users.noreply.github.com>
2018-10-04 09:10:03 +0300

    Add files via upload

---
+++ zdravei.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"Zdravei! Kak se kazvash?";
+    char ime [15];
+    cin>>ime;
+    cout<<"Zdrasti! Az se kazvam "<<ime<<"!"<<endl;
+    return 0;
+}
